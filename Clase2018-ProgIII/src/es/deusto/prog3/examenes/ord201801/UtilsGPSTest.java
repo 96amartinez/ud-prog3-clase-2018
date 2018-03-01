42de6544e3d584036716e18814833e0c3f443148
Andoni Eguiluz <andoni.eguiluz@deusto.es>
2018-03-01 09:33:19 +0100

    Exámenes de ejemplo - capítulo 1

--- Clase2018-ProgIII/src/es/deusto/prog3/examenes/ord201801/UtilsGPSTest.java
+++ Clase2018-ProgIII/src/es/deusto/prog3/examenes/ord201801/UtilsGPSTest.java
@@ -2,6 +2,9 @@ package es.deusto.prog3.examenes.ord201801;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.junit.Test;
 
 public class UtilsGPSTest {
@@ -11,4 +14,17 @@ public class UtilsGPSTest {
 		// TAREA 1
 	}
 
+		// Método para comprobar todas las zonas
+		// (no se daba en el examen original)
+		private boolean puntoEstaEnAlgunaZona( PuntoGPS punto ) {
+			Iterator<Zona> itZona = GrupoZonas.jardinesErandio.getIteradorZonas();
+			while (itZona.hasNext()) {
+				Zona zona = itZona.next();
+				for (ArrayList<PuntoGPS> subzona : zona.getPuntosGPS()) {
+					if (UtilsGPS.gpsDentroDePoligono( punto, subzona )) return true;
+				}
+			}
+			return false;
+		}
+	
 }
