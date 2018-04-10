f98d889fc94f8cb65a660e389c66156474a8c721
Andoni Eguiluz <andoni.eguiluz@deusto.es>
2018-04-10 20:26:00 +0200

    Capítulo 4 en clase

--- Clase2018-ProgIII/src/es/deusto/prog3/cap04/PruebasRecursivas.java
+++ Clase2018-ProgIII/src/es/deusto/prog3/cap04/PruebasRecursivas.java
@@ -3,36 +3,34 @@ package es.deusto.prog3.cap04;
 public class PruebasRecursivas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//f(0);
-		System.out.println( factorial(12) );
-		System.out.println( string1_10_1(1) );
-		System.out.println( 1/c(32) );
+		// Pruebas recursivas iniciales
+		//f(0);  // Recursividad simplona
+		System.out.println( factorial(12) );  // Factorial recursivo
+		System.out.println( string1_10_1(1) );  // Visualizar números 1 al 10 al derecho y al revés
+		System.out.println( c(10) );  // Ejemplo de división /4 recursiva
+		System.out.println( fib(45) );
 	}
 	
-		private static double c(int n) { if (n==1) return 1/4.0; else return c(n-1)/4.0; } 
-	
-	private static void f(int i) {
-		if (i<1000)
-			f(i+1);
-		// else caso base
-		System.out.println( i );
-	}
-	
-	/** Devuelve el factorial de un número
-	 * @param n	Número mayor o igual a 0
-	 * @return	Factorial de n
+	/** Devuelve el enésimo número de Fibonacci
+	 * Usando la definición recursiva
+	 * fib(n) = fib(n-1) + fib(n-2)  si n>2
+	 *        = 1                    si n==1 o n==2
+	 * @param n	Número del cual calcular la función
+	 * @return	Valor de la función fib(n)
 	 */
-	private static long factorial(long n) {
-		// Definición factorial:
-		// factorial(n) = n * factorial(n-1)  si n>0
-		// factorial(n) = 1 si n==0
-		if (n==0)
-			return 1;
-		else
-			return n * factorial(n-1);
+	private static long fib( long n ) {
+		if (n==1) {
+			return 1;   // Caso base 1
+		} else if (n==2) {
+			return 1;   // Caso base 2
+		} else {
+			return fib(n-1) + fib(n-2);
+		}
 	}
-
+	
+	// Ejemplo de división recursiva
+	private static double c(int n) { if (n==1) return 1/4.0; else return c(n-1)/4.0; } 
+	
 	/** Devuelve un string formado de todos los números
 	 * subiendo entre n y 10 y bajando de nuevo hasta n
 	 * Ejemplo: "1 2 3 ... 9 10 9 ... 3 2 1". 
@@ -48,4 +46,28 @@ public class PruebasRecursivas {
 		else
 			return n + " " + string1_10_1(n+1) + " " + n;
 	}
+
+	/** Devuelve el factorial de un número
+	 * @param n	Número mayor o igual a 0
+	 * @return	Factorial de n
+	 */
+	private static long factorial(long n) {
+		// Definición factorial:
+		// factorial(n) = n * factorial(n-1)  si n>0
+		// factorial(n) = 1 si n==0
+		if (n==0)
+			return 1;
+		else
+			return n * factorial(n-1);
+	}
+
+	// Recursividad simplona
+	private static void f(int i) {
+		if (i<1000)
+			f(i+1);
+		// else caso base
+		System.out.println( i );
+	}
+		
+	
 }
